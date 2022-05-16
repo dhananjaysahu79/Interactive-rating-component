06a302f2297317dcc3c701f6fbd6822a59ed1c51
Dhananjay <49968050+dhananjaysahu79@users.noreply.github.com>
2022-05-16 18:38:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
+import starIcon from './icon-star.svg';
+import thanks from './illustration-thank-you.svg';
 import './App.css';
-
+import React, {useState, useEffect} from 'react';
 function App() {
+  const [isClicked, setClick] = useState(false);
+  const [feedback, setFeedback] = useState(1);
+
+  const handleFeedback = value => { 
+    setFeedback(value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!isClicked ?
+        <div className = "Card" id = 'card1'>
+          <div className = "wrapper-text">
+            <div className = "circle">
+              <img src = {starIcon}></img>
+            </div>
+            <h2>How did we do?</h2>
+            <p>
+            Please let us know how we did with your support request. All feedback is appreciated 
+  to help us improve our offering!
+            </p>
+          </div>
+          <div className = "rating-circles-container">
+             {
+              [1,2,3,4,5].map((value) => 
+              <button className = "circle" id = 'btn2' onClick= {() => handleFeedback(value)} >
+                {value}
+              </button>)
+             }
+          </div>
+          <button className = "submitButton" onClick = {setClick}>
+            SUBMIT
+          </button>
+        </div>
+
+        :
+
+        <div className = "Card" id = 'card2'>
+        <img src = {thanks}></img>
+        <div className='RatingPill'>
+            <p className='Ratingtext'>You selected {feedback} out of 5</p>
+        </div>
+        <h2>Thank you!</h2>
+          <p>
+          We appreciate you taking the time to give a rating. If you ever need more support, 
+  don’t hesitate to get in touch!
+          </p>
+        </div>
+      }
       </header>
     </div>
   );
